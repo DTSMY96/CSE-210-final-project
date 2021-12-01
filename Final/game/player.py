c66ca3693e03f3065c589a5fcac31fccb0d4d656
youngm <dizzytmy@byui.edu>
2021-12-01 10:59:20 -0700

    made changes to many classes

--- Final/game/player.py
+++ Final/game/player.py
@@ -1,8 +1,9 @@
 from game.actor import actor
+from game.point import point
 from game import constants
 
 class player(actor):
     def __init__(self):
         super().__init__()
 
-        self.set_width
+        
